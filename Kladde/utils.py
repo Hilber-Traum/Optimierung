68cbbedfe31a5843472e387e796bfa423c84615f
Mika Meinert Kohlhammer <69604540+Bar0nVeng30us@users.noreply.github.com>
2025-06-12 12:28:49 +0200

    Add files via upload

--- Kladde/utils.py
+++ Kladde/utils.py
@@ -1,8 +1,107 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_iteration_rosenbrock(log, title, rosenbrock):
+# Uebernommen von Blatt 8
+def armijo(f, p, x, rho, c1, alpha0=1):
+    """
+    Armijo Backtracking Liniensuche
+
+    Input:
+        f: Funktion, wobei f(x) = (val, grad, hess),
+        p: Vektor (Abstiegsrichtung)
+        x: Vektor (Iterierte)
+        rho: Skalar (Reduktionsfaktor für alpha)
+        c1: Skalar (Parameter für die Armijo-Bedingung)
+        alpha0: Skalar (Initial getesteter Startwert)
+
+    Output:
+        alpha: Skalar (gesuchte Schrittweite)
+    """
+
+    # Initialisierung
+    alpha = alpha0
+
+    # Vorberechnung reduziert Laufzeit in der Schleife
+    valk, gradk, _ = f(x)
+    descent = gradk @ p
+
+    for lsiter in range(100):
+        valkp1, _, _ = f(x + alpha * p)
+
+        # Testen der ``sufficient decrease condition``
+        if valkp1 <= valk + c1 * alpha * descent:
+            return alpha
+
+        # Reduzieren von alpha
+        alpha = rho * alpha
+
+    # Fehlermeldung, falls die Liniensuche nicht auskonvergiert ist
+    print("Liniensuche wurde nach 100 Schritten frühzeitig abgebrochen.")
+    return alpha
+
+
+# Uebernommen von Blatt 8
+def gill_murray_wright(xk, xkplus1, valk, valkplus1, gradk, k, eps, tau, kmax):
+    """
+    Abbruchkriterien nach Gill, Murray und Wright
+
+    Input:
+        xk: Vektor (aktuelle Iterierte)
+        xkp1: Vektor (nächste Iterierte)
+        valk: Skalar (f(xk))
+        valkp1: Skalar (f(xkp1))
+        gradk: Vektor (\nabla f(xk))
+        k: Integer (Iteration-Nummer)
+        tau: Skalar (Tolerenz für Kriterium 1)
+        eps: Skalar (Tolerenz für Gradient -- Kriterium 3)
+        kmax: Integer (maximale Anzahl der Iterationen)
+
+    Output:
+        test: Boolescher Wert (Ergebnis der Abbruchkriterien nach Gill, Murray, Wright)
+    """
+
+    test = False
+    if (valk - valkplus1 < tau * (1 + np.abs(valk))
+            and np.linalg.norm(xkplus1 - xk) < np.sqrt(tau) * (1 + np.linalg.norm(xk))
+            and np.linalg.norm(gradk) < np.cbrt(tau) * (1 + np.abs(valk))):
+        test = True
+        print('\nAbbruch nach GMW 1 erfuellt')
+
+    if np.linalg.norm(gradk) < eps:
+        test = True
+        print('\nAbbruch nach GMW 3 erfuellt')
+
+    if k == kmax:
+        test = True
+        print('\nAbbruch nach GMW 3 erfuellt')
+
+    return test
+
+
+# Uebernommen von Blatt 8
+def rosenbrock(x, y):
+    """
+    Implementierung der Rosenbrock-Funktion f(x, y) = 100(y-x**3)**3 + (1-x)**3
+
+    Input:
+        x, y: Skalare bzw. Arrays zur Auswertung der Funktion.
+
+    Output:
+        val: Zielfunktionswert(e),
+        grad: Gradient(en),
+        hess: Hessematri(x/zen).
+    """
+    val = 100 * (y - x ** 2) ** 2 + (1 - x) ** 2
+    grad = np.stack((- 400 * x * (y - x ** 2) - 2 * (1 - x), 200 * (y - x ** 2)), axis=-1)
+    hess = np.stack((np.stack((-400 * (y - x ** 2) + 800 * x ** 2 + 2, -400 * x), axis=-1),
+                     np.stack((-400 * x, 200 * np.ones_like(x)), axis=-1)), axis=1)
+
+    return val, grad, hess
+
+
+# Anpepasst von Blatt 8
+def plot_iteration_rosenbrock(log, title):
     """
     Funktion zur Erstellung von Plots zum Optimierungsverlauf der Rosenbrock-Funktion anhand der Ergebnisse in log.
     Im ersten Plot wird der Iterationsverlauf auf einem Surface-Plot dargestellt, in weiteren 3 Konvergenzeigenschaften.
